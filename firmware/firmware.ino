fb346d271ec14c715138772aa8a3e28fa6322339
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2023-05-29 11:41:50 +0530

    Firmware: WiFi provisioning with QR done - Added necessary arduino library dependencies - Added display related configs - Added useful references

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -13,15 +13,25 @@
 #include "WiFi.h"
 #include "esp_wifi.h"
 
+#include <SPI.h>
+#include <TFT_eSPI.h>
+#include <qrcode_espi.h>
+
 #define USERNAME "user"
 
 int reconnect_try = 0;
+bool is_prov_needed = false;
+bool is_qr_displayed = false;
+
+TFT_eSPI display = TFT_eSPI();
+QRcode_eSPI qrcode (&display);
 
 /**
  * @brief Function that erases WiFi credentials
  * 
  */
-void eraseAllWiFiCredentials() {
+void eraseAllWiFiCredentials()
+{
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 	esp_wifi_init(&cfg); //initiate and allocate wifi resources (does not matter if connection fails)
 	delay(2000); //wait a bit
@@ -37,6 +47,24 @@ void eraseAllWiFiCredentials() {
 	delay(1000);
 
 	ESP.restart();
+}
+
+/**
+ * @brief Function to display the QR code when provisioning is needed
+ * 
+ */
+void displayProvQRcode()
+{
+	qrcode.init();
+
+	String qr_prov = "{\"ver\":\"v1\",\"name\":\"PROV_BIGBYE\",\"pop\":\"";
+	qr_prov += USERNAME;
+	qr_prov += "\",\"transport\":\"softap\"}";
+	qrcode.create(qr_prov);
+
+	display.setTextSize(1);
+	display.setTextColor(TFT_BLACK, TFT_WHITE);
+	display.drawString("Scan with ESP SoftAP Prov", 5, 5, 1);
 }
 
 /**
@@ -68,6 +96,7 @@ void SysProvEvent(arduino_event_t *sys_event)
 
 		case ARDUINO_EVENT_PROV_START:
 			Serial.println("\nProvisioning started\nGive Credentials of your access point using \" Android app \"");
+			is_prov_needed = true;
 			break;
 
 		case ARDUINO_EVENT_PROV_CRED_RECV:
@@ -91,6 +120,7 @@ void SysProvEvent(arduino_event_t *sys_event)
 			break;
 		case ARDUINO_EVENT_PROV_END:
 			Serial.println("\nProvisioning Ends");
+			is_prov_needed = false;
 			break;
 		default:
 			break;
@@ -107,7 +137,12 @@ void setup()
 
 	// WiFi 
 	WiFi.onEvent(SysProvEvent);
-	WiFiProv.beginProvision(WIFI_PROV_SCHEME_SOFTAP, WIFI_PROV_SCHEME_HANDLER_NONE, WIFI_PROV_SECURITY_1, USERNAME, "Prov_BIGBYE");
+	WiFiProv.beginProvision(WIFI_PROV_SCHEME_SOFTAP, WIFI_PROV_SCHEME_HANDLER_NONE, WIFI_PROV_SECURITY_1, USERNAME, "PROV_BIGBYE");
+
+	// Diplay initialisation
+	display.init();
+	display.setRotation(3);
+	display.fillScreen(TFT_WHITE);
 }
 
 /**
@@ -115,4 +150,20 @@ void setup()
  */
 void loop()
 {
+	if(is_prov_needed && !is_qr_displayed)
+	{
+		displayProvQRcode();
+		is_qr_displayed = true;
+	}
+	
+	else if(!is_prov_needed)
+	{
+		is_qr_displayed = false;
+		display.fillScreen(TFT_WHITE);
+		display.setTextSize(1);
+		display.setTextColor(TFT_BLACK, TFT_WHITE);
+		display.drawString("Text from internet here.", 5, 64, 1);
+	}
+	
+	delay(2000);
 }
